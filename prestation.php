b172ce4de1c618eacb715f928c31e9a0b0e1dc4e
simplon <patrickdealmeida89000@gmail.com>
2016-08-16 15:11:13 +0200

    Ajout des page

---
+++ prestation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Trek & co</title>
+  </head>
+  <body>
+    <ul class="topnav">
+      <li class="left"><a href="index.php" content:"f06d">Home</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="trek.php">Trek</a></li>
+      <li><a class="active" href="prestation.php">Prestation</a></li>
+    </ul>
+
+    <div class="contenu">
+
+
+    </div>
+
+
+
+
+
+    <footer>
+      <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+      <span class="sr-only">Loading...</span>
+    </footer>
+  </body>
+</html>
